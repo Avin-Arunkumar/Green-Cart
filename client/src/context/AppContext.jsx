a696beed4aa5229a9f686f54ff1adc524128858d
Avin-Arunkumar <avinarun2005@gmail.com>
2025-04-30 18:59:35 +0530

    All Product

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
   // Fetch All Product
   const fetchProducts = async () => {
     setProducts(dummyProducts);
