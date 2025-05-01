709dc9eec82ba702ac07531861eabca84e59d610
Avin-Arunkumar <avinarun2005@gmail.com>
2025-05-01 19:20:04 +0530

    Updated AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -50,6 +50,27 @@ export const AppContextProvider = ({ children }) => {
     toast.success("Removed from Cart");
     setCartItems(cartData);
   };
+
+  // Get Cart Item Count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+  // Get cart Total Amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerprice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -69,6 +90,8 @@ export const AppContextProvider = ({ children }) => {
     cartItems,
     searchQuery,
     setSearchQuery,
+    getCartAmount,
+    getCartCount,
   };
   return <AppContent.Provider value={value}>{children}</AppContent.Provider>;
 };
