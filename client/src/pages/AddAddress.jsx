f4608a8f5b4ab76783dd17550a02d8cf1c919e82
Avin-Arunkumar <avinarun2005@gmail.com>
2025-05-03 13:54:47 +0530

    Final Commit

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,8 +1,127 @@
-import React from "react";
-const inputField = () => <input type="text" />;
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+    type={type}
+    placeholder={placeholder}
+    onChange={handleChange}
+    name={name}
+    value={address[name]}
+    required
+  />
+);
 
 const AddAddress = () => {
-  return <div></div>;
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAddress((prevAddress) => ({
+      ...AddAddress,
+      [name]: value,
+    }));
+  };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="mt-16 pb-16">
+      <p className="text-2xl md:text-3xl text-gray-500">
+        Add Shipping <span className="font-semibold text-primary">Address</span>
+      </p>
+      <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+        <div className="flex-1 max-w-md">
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="lastName"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="email"
+              type="email"
+              placeholder="Email address"
+            />
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="street"
+              type="text"
+              placeholder="Enter your Street"
+            />
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="city"
+                type="text"
+                placeholder="Enter your City"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="State"
+                type="text"
+                placeholder="Enter your State"
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="pinCode"
+                type="number"
+                placeholder="Enter your PinCode"
+                className="input-no-spinner border border-green-400 rounded px-2 py-1"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="Country"
+                type="text"
+                placeholder="Enter your Country "
+              />
+            </div>
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="PhoneNumber"
+              type="text"
+              placeholder="Phone Number "
+            />
+          </form>
+        </div>
+        <img
+          className="md:mr-0 mb-6 md:mt-0"
+          src={assets.add_address_iamge}
+          alt="Add-Address"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AddAddress;
