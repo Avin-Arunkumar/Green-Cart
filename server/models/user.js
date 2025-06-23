84cddbfd55bb1d674f0d42c8042559bc9083d33d
Avin-Arunkumar <avinarun2005@gmail.com>
2025-06-23 22:53:46 +0530

    UserController Update

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.SchemaType(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.models.User || mongoose.model("user", userScheme);
+
+export default User;
