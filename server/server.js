ba94826c6ff9bf08e8efd513181a695f43c55eea
Avin-Arunkumar <avinarun2005@gmail.com>
2025-07-01 10:11:25 +0530

    setup the seller Router

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
 
 app.listen(port, () => {
   console.log(`server is in running in http://localhost:${port}`);
