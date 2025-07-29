6b90173b9ce8053a41620c1507bd112071810e01
Avin-Arunkumar <avinarun2005@gmail.com>
2025-07-29 22:11:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Green-Cart
+
+🥬 Green Cart – Online Vegetable Shopping Website
+# Green-Cart2
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,177 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import toast, { ToastBar } from "react-hot-toast";
+import axios from "axios";
+
+export const AppContent = createContext();
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+
+  const [searchQuery, setSearchQuery] = useState({});
+
+  // Add to Cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to Cart");
+  };
+
+  // Update Cart Item Quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart Updated");
+  };
+
+  // Remove Product from Cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Removed from Cart");
+    setCartItems(cartData);
+  };
+
+  // Get Cart Item Count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  // Get Cart Total Amount
+  const getCartAmount = () => {
+    let total = 0;
+    for (const key in cartItems) {
+      const product = products.find(
+        (item) =>
+          String(item.id) === String(key) || String(item._id) === String(key)
+      );
+      if (product && cartItems[key] > 0) {
+        total += (product.offerPrice || 0) * cartItems[key];
+      }
+    }
+    return total;
+  };
+
+  // Fetch Seller Status
+  const fetchSeller = async () => {
+    try {
+      const { data } = await axios.get("/api/seller/is-auth");
+      if (data.success) {
+        setIsSeller(true);
+      } else {
+        setIsSeller(false);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        setIsSeller(false);
+      } else {
+        toast.error(error.message);
+      }
+    }
+  };
+  // Fetch User Auth Status,User Data and Cart Items
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("api/user/is-auth");
+      if (data.success) {
+        setUser(data.user);
+        setCartItems(data.user.cartItems);
+      }
+    } catch (error) {
+      setUser(null);
+    }
+  };
+
+  // Fetch All Products
+  const fetchProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/product/list");
+      if (data.success) {
+        setProducts(data.products);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+    fetchProducts();
+    fetchSeller();
+  }, []);
+
+  //Update Database Cart Items
+  useEffect(() => {
+    const updateCart = async () => {
+      try {
+        const { data } = await axios.post("api/cart/update", { cartItems });
+        if (!data.success) {
+          toast.error(data.message);
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+
+    if (user) {
+      updateCart();
+    }
+  }, [cartItems]);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    setIsSeller,
+    isSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    removeFromCart,
+    updateCartItem,
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartAmount,
+    getCartCount,
+    axios,
+    fetchProducts,
+    setCartItems,
+  };
+
+  return <AppContent.Provider value={value}>{children}</AppContent.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContent);
+};
